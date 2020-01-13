c1af82945ae00e82fbe34f8059b522195e58e5e3
Praveenkumar <praveen1050208@gmail.com>
2020-01-13 09:14:55 +0530

    Update input_path.py
    chkpt path changed

--- scripts/input_path.py
+++ scripts/input_path.py
@@ -2,16 +2,16 @@
 from bunch import Bunch
 import os
 
-#core_path = '/content/drive/My Drive/Client_demo/'  #G_drive
+#g_drive_path = '/content/drive/My Drive/Client_demo/'  #G_drive
 core_path = os.getcwd() 
 dataset_name = 'cnn'
 g_drive_vocab_path = '/content/drive/My Drive/Client_demo/input_files/Vocab_files'
 file_path = {
-        'best_ckpt_path' : os.path.join(core_path, "created_files/training_summarization_model_ckpts/"+dataset_name+"/best_checkpoints/"),  
-        'checkpoint_path' : os.path.join(core_path, dataset_name+"_checkpoints"),
+        'best_ckpt_path' : "/content/drive/My Drive/Text_summarization/BERT_text_summarisation/created_files/training_summarization_model_ckpts/cnn/best_checkpoints",  
+        'checkpoint_path' : "/content/drive/My Drive/Text_summarization/BERT_text_summarisation/cnn_checkpoints",
         'G_drive_vocab_path' : g_drive_vocab_path,
-        'infer_csv_path' : os.path.join(core_path, "input_files/Azure_dataset/Test.csv"),
-        'infer_ckpt_path' : os.path.join(core_path, "created_files/training_summarization_model_ckpts/"+dataset_name+"/best_checkpoints/ckpt-37"),
+        'infer_csv_path' : os.path.join(core_path, "BERT_text_summarisation/input_files/Azure_dataset/Test.csv"),
+        'infer_ckpt_path' : "/content/drive/My Drive/Text_summarization/BERT_text_summarisation/cnn_checkpoints/ckpt-1",
         'log_path' : os.path.join(core_path, "created_files/tensorflow.log"),
         'subword_vocab_path' : os.path.join(core_path, "input_files/vocab_file_summarization_"+dataset_name),
         'summary_write_path' : os.path.join(core_path, "created_files/summaries/"+dataset_name+"/"),
