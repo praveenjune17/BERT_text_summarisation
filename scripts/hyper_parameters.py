c004ee8c5882af76d65c00484695474dadd6d79f
Praveenkumar <praveen1050208@gmail.com>
2020-01-20 09:45:57 +0530

    Update hyper_parameters.py

--- scripts/hyper_parameters.py
+++ scripts/hyper_parameters.py
@@ -7,18 +7,18 @@ for device in gpu_devices:
     tf.config.experimental.set_memory_growth(device, True)
 
 hyp = {
-	 'accumulation_steps': 36,                                                                                  
+	 'accumulation_steps': 36,                                                                                   
 	 'batch_size': 1,
-	 'beam_sizes': [2, 3, 4],        # Used only during inference                                                 
+	 'beam_sizes': [2, 3, 4],        	     # Used only during inference                                                 
 	 'combined_metric_weights': [0.4, 0.3, 0.3], #(bert_score, rouge, validation accuracy)
-	 'dropout_rate': 0.15,
+	 'dropout_rate': 0.0,
 	 'epochs': 4,
-	 'epsilon_ls': 0.1,              # label_smoothing hyper parameter
+	 'epsilon_ls': 0.0,              	     # label_smoothing hyper parameter
 	 'grad_clipnorm':None,
 	 'l2_norm':0,
-	 'learning_rate': 3e-4,          # set learning rate decay
-	 'length_penalty' : 1,
-	 'mean_attention_heads':True,    # if False then the attention weight of the last head will be used
+	 'learning_rate': None,          	     # change to None to set learning rate decay
+	 'length_penalty' : 1,                       # Beam search hyps . Used only during inference                                                 
+	 'mean_attention_heads':True,                # if False then the attention weight of the last head will be used
 	 }                                    
 
 h_parms = Bunch(hyp)
