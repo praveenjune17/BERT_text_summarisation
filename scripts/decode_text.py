e9cadfbcc28d48c4c387ceb2348fa34958e12cc1
Praveenkumar <praveen1050208@gmail.com>
2020-01-22 18:16:49 +0530

    'New_decoder_scripts_Added'

--- scripts/decode_text.py
+++ scripts/decode_text.py
@@ -1,8 +1,11 @@
 #code adapted from https://github.com/ShenakhtPajouh/GPT-language-model-tf.keras/blob/master/utils.py#L126
+import tensorflow as tf
+tf.random.set_seed(100)
 
-def top_k_sampling(logits, k=25, temperature=0.8):
+
+def top_k_sampling(logits, k=25, temperature=0.9):
     'k must be greater than 0'
-
+    logits = tf.squeeze(logits, 0)
     values, _ = tf.nn.top_k(logits, k=k)
     min_value = tf.reduce_min(values)
     logits = tf.where(
@@ -11,16 +14,16 @@ def top_k_sampling(logits, k=25, temperature=0.8):
         logits)
 
     logits = logits / temperature
-
-    sample = tf.multinomial(tf.expand_dims(logits, 0), num_samples=1, output_dtype=tf.int32)
-    return tf.reduce_sum(sample)
+    sample = tf.random.categorical(logits, num_samples=1, dtype=tf.int32, seed=1)
+    return sample
 
 
 def argmax(logits):
-    return tf.argmax(logits)
+    return tf.argmax(logits, axis=-1)
 
-
+#Needs to be fixed
 def nucleus_sampling(logits, p=0.9):
+    logits = tf.squeeze(logits, 0)
     sorted_logits = tf.sort(logits, direction='DESCENDING')
     sorted_indices = tf.argsort(logits, direction='DESCENDING')
     cumulative_probs = tf.cumsum(tf.nn.softmax(sorted_logits))
@@ -36,12 +39,12 @@ def nucleus_sampling(logits, p=0.9):
         tf.ones_like(logits, dtype=logits.dtype) * -1e10,
         logits
     )
-
-    sample = tf.multinomial(tf.expand_dims(logits, 0), num_samples=1, output_dtype=tf.int32)
-    return tf.reduce_sum(sample)
+    sample = tf.random.categorical(logits, num_samples=1, dtype=tf.int32, seed=1)
+    return sample
 
 
 def sampling(logits, temperature=0.8):
+    logits = tf.squeeze(logits, 0)
     logits = logits / temperature
-    sample = tf.multinomial(tf.expand_dims(logits, 0), num_samples=1, output_dtype=tf.int32)
-    return tf.reduce_sum(sample)
+    sample = tf.random.categorical(logits, num_samples=1, dtype=tf.int32, seed=1)
+    return sample
