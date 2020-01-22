fe2d2a343f5f49b11d8e9f69fc2d3b15a5c99285
Praveenkumar <praveen1050208@gmail.com>
2020-01-22 09:59:25 +0530

    Update train_bert_summarizer_mixed_precision.py

--- scripts/train_bert_summarizer_mixed_precision.py
+++ scripts/train_bert_summarizer_mixed_precision.py
@@ -17,6 +17,24 @@ from creates import log, train_summary_writer, valid_summary_writer
 from create_tokenizer import tokenizer, model
 from local_tf_ops import *
 
+def check_ckpt(checkpoint_path):
+    ckpt = tf.train.Checkpoint(
+                               model=model,
+                               optimizer=optimizer
+                              )
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=10)
+    if tf.train.latest_checkpoint(checkpoint_path):
+      ckpt.restore(ckpt_manager.latest_checkpoint)
+      log.info(ckpt_manager.latest_checkpoint +' restored')
+      latest_ckpt = int(ckpt_manager.latest_checkpoint[-2:])
+    else:
+        latest_ckpt=0
+        log.info('Training from scratch')
+    return (ckpt_manager, latest_ckpt)
+
+# if a checkpoint exists, restore the latest checkpoint.
+ck_pt_mgr, latest_ckpt = check_ckpt(file_path.checkpoint_path)
+
 policy = mixed_precision.Policy('mixed_float16')
 mixed_precision.set_policy(policy)
 optimizer = mixed_precision.LossScaleOptimizer(optimizer, loss_scale='dynamic')
@@ -98,23 +116,7 @@ def val_step(input_ids,
     rouge, bert = (1.0, 1.0)  
   return (rouge, bert)
 
-def check_ckpt(checkpoint_path):
-    ckpt = tf.train.Checkpoint(
-                               model=model,
-                               optimizer=optimizer
-                              )
-    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=10)
-    if tf.train.latest_checkpoint(checkpoint_path):
-      ckpt.restore(ckpt_manager.latest_checkpoint)
-      log.info(ckpt_manager.latest_checkpoint +' restored')
-      latest_ckpt = int(ckpt_manager.latest_checkpoint[-2:])
-    else:
-        latest_ckpt=0
-        log.info('Training from scratch')
-    return (ckpt_manager, latest_ckpt)
 
-# if a checkpoint exists, restore the latest checkpoint.
-ck_pt_mgr, latest_ckpt = check_ckpt(file_path.checkpoint_path)
 total_steps = int(h_parms.epochs * (h_parms.accumulation_steps))
 train_dataset = train_dataset.repeat(total_steps)
 count=0
@@ -187,4 +189,4 @@ for (step, (input_ids, input_mask, input_segment_ids, target_ids_, target_mask, 
                          rouge_score, 
                          valid_summary_writer, 
                          step+1):
-        break  
+        break
