e1b6bac0401dfb6b910aa3387fecc10da841a1f4
Praveenkumar <praveen1050208@gmail.com>
2020-02-03 07:35:21 +0530

    Update decode_text_v2.py
    added topk-topk sampling

--- utils/decode_text_v2.py
+++ utils/decode_text_v2.py
@@ -109,9 +109,34 @@ def nucleus_sampling(logits, p=0.9):
     return sample
 
 def topp_topk(logits, p, k):
-  logits = nucleus_sampling(logits, p=p)
-  logits = top_k_sampling(logits, k=k)
-  return logits
+  sorted_logits = tf.sort(logits, direction='DESCENDING')
+  sorted_indices = tf.argsort(logits, direction='DESCENDING')
+  cumulative_probs = tf.cumsum(tf.nn.softmax(sorted_logits))
+  t_sorted_indices_to_remove = cumulative_probs > p
+  ''' Shift the indices to the right to keep also the first token above the threshold '''
+  indices = tf.range(1, tf.shape(logits)[0], 1)
+  sorted_indices_to_remove = tf.scatter_nd(
+                                           tf.expand_dims(indices, 1), 
+                                           t_sorted_indices_to_remove[:-1], 
+                                           logits.shape
+                                           )
+  # indices_to_remove = tf.boolean_mask(sorted_indices, sorted_indices_to_remove)
+  # t = tf.ones(tf.shape(indices_to_remove)[0], dtype=tf.bool)
+  # to_remove = tf.scatter_nd(indices_to_remove, t, logits.shape)
+  logits = tf.where(
+      sorted_indices_to_remove,
+      tf.ones_like(logits, dtype=logits.dtype) * -1e10,
+      logits
+  )
+  values, _ = tf.nn.top_k(logits, k=k)
+  min_value = tf.reduce_min(values)
+  logits = tf.where(
+      logits < min_value,
+      tf.ones_like(logits, dtype=logits.dtype) * -1e10,
+      logits)
+  logits = tf.reshape(logits, (h_parms.batch_size, -1))
+  sample = tf.random.categorical(logits, num_samples=1, dtype=tf.int32, seed=1)
+  return sample
 
 def draft_summary_sampling(
                            inp, 
